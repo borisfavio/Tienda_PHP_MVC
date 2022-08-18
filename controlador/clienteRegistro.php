1940e7cdc27408e32ce4c92d9fbb79d167c2ddb6
borisfavio <46292075+borisfavio@users.noreply.github.com>
2022-08-18 17:06:28 -0400

    nuevo cambio

--- controlador/clienteRegistro.php
+++ controlador/clienteRegistro.php
@@ -1,12 +1,24 @@
 <?php
 include("../vista/clienteRegistro.php");
 if (isset($_POST['registrarCliente'])) {
-    $ni =$_POST['nit'];
-    $rs =$_POST['razon'];
+    $ni = $_POST['nit'];
+    $rs = $_POST['razon'];
     $es = "activo";
     include('../modelo/clienteClase.php');
-    $cli = new Cliente("",$ni,$rs,$es);
+    $cli = new Cliente("", $ni, $rs, $es);
     $r = $cli->grabarCliente();
-    
+    if ($r) {
+?>
+        <script type="text/javascript">
+            alert("Se registro correctamente");
+        </script>
+    <?php
+    } else {
+    ?>
+        <script type="text/javascript">
+            alert("No se registro");
+        </script>
+<?php
+    }
 }
 ?>
